0a00883e0d87fa7dd2381ffaf64208dbbc1a8d43
yichenlionelwu <lionelwuyichen@gmail.com>
2024-01-07 01:32:54 -0800

    rapid multiplication

---
+++ src/Root.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Root() {
+  return (
+    <div>
+        <p>My portfolio</p>
+        <a href={"rapid_multiplication"}>rapid multiplication</a>
+    </div>
+  );
+}
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Root from "./Root";
+import RapidMultiplication from "./pages/RapidMultiplication";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+    },
+    {
+        path: "/rapid_multiplication/",
+        element: <RapidMultiplication/>
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
